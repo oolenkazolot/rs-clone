870ab729cee15239c6c194fb087940bac8223f23
Volha Haiduk <Alisatonks2385@gmail.com>
2023-02-05 00:07:40 +0100

    feat: add translation to russian language of workout-plans file

---
+++ src/utils/workout-plans -ru.ts
@@ -0,0 +1,99 @@
+const workout_plans = [
+  {
+    title: "ABS Тренировки",
+    block: [
+      {
+        title: "ABS новичок",
+        exercisesAmt: "19",
+        exercisesTime: "15",
+        complexityLevel: true,
+      },
+      {
+        title: "ABS средний",
+        exercisesAmt: "20",
+        exercisesTime: "17",
+        complexityLevel: true,
+      },
+      {
+        title: "ABS продвинутый",
+        exercisesAmt: "26",
+        exercisesTime: "13",
+        complexityLevel: true,
+      },
+    ],
+  },
+  {
+    title: "Тренировки для ягодиц",
+    block: [
+      {
+        title: "Ягодицы новичок",
+        exercisesAmt: "18",
+        exercisesTime: "13",
+        complexityLevel: true,
+      },
+      {
+        title: "Ягодицы средний",
+        exercisesAmt: "26",
+        exercisesTime: "18",
+        complexityLevel: true,
+      },
+      {
+        title: "Ягодицы продвинутый",
+        exercisesAmt: "32",
+        exercisesTime: "24",
+        complexityLevel: true,
+      },
+    ],
+  },
+  {
+    title: "Тренировки для бедер",
+    block: [
+      {
+        title: "Бедра новичок",
+        exercisesAmt: "19",
+        exercisesTime: "14",
+        complexityLevel: true,
+      },
+      {
+        title: "Бедра средний",
+        exercisesAmt: "26",
+        exercisesTime: "19",
+        complexityLevel: true,
+      },
+      {
+        title: "Бедра продвинутый",
+        exercisesAmt: "29",
+        exercisesTime: "21",
+        complexityLevel: true,
+      },
+    ],
+  },
+  {
+    title: "Повседневные тренировки",
+    block: [
+      {
+        title: "Утренний разогрев",
+        exercisesAmt: "11",
+        exercisesTime: "10",
+      },
+      {
+        title: "Растяжки перед сном",
+        exercisesAmt: "13",
+        exercisesTime: "13",
+      },
+    ],
+  },
+  {
+    title: `7 x 4 Испытание`,
+    block: [
+      {
+        title: "тренировка на все тело",
+        exercisesAmt: "16 - 24",
+        exercisesTime: "13 - 22",
+        complexityLevel: true,
+      },
+    ],
+  },
+];
+
+export default workout_plans;

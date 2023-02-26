ea3fc17f3c0798bdfdf57fb486fa8ecddb832257
oolenkazolot <oolenka.zolot@gmail.com>
2023-02-26 20:01:04 +0100

    fix: fix in startTraining, header, exerciseBlock

--- src/components/exerciseBlock.ts
+++ src/components/exerciseBlock.ts
@@ -1,6 +1,6 @@
-import Template from '../templates/template';
-import { ITemplate, IExercise } from '../types/index';
-import PauseModal from '../components/pauseModal';
+import Template from "../templates/template";
+import { ITemplate, IExercise } from "../types/index";
+import PauseModal from "../components/pauseModal";
 
 class ExerciseBlock {
   template: ITemplate;
@@ -11,16 +11,19 @@ class ExerciseBlock {
 
   constructor(exercise: IExercise) {
     this.template = new Template();
-    this.exerciseBlock = this.template.createElement('div', 'exercise-block');
-    this.counter = this.template.createElement('div', 'exercise-block__counter');
+    this.exerciseBlock = this.template.createElement("div", "exercise-block");
+    this.counter = this.template.createElement(
+      "div",
+      "exercise-block__counter"
+    );
     this.exercise = exercise;
     this.interval;
   }
 
   public draw(): HTMLElement {
     this.createBlocksHeader();
-    const exerciseGif: HTMLImageElement = document.createElement('img');
-    exerciseGif.className = 'exercise-block__gif';
+    const exerciseGif: HTMLImageElement = document.createElement("img");
+    exerciseGif.className = "exercise-block__gif";
     const path: string = this.exercise.example;
     exerciseGif.src = path;
     this.exerciseBlock.append(exerciseGif);
@@ -40,34 +43,71 @@ class ExerciseBlock {
   // }
 
   private createBlocksHeader(): void {
-    const blockHeader: HTMLElement = this.template.createElement('div', 'exercise-block__header');
+    const blockHeader: HTMLElement = this.template.createElement(
+      "div",
+      "exercise-block__header"
+    );
     this.exerciseBlock.append(blockHeader);
-    const returnButton: HTMLButtonElement = this.template.createBtn('exercise-block__button-return');
-    const settings: HTMLElement = this.template.createElement('div', 'exercise-block__settings-block');
-    const volumeButton: HTMLButtonElement = this.template.createBtn('exercise-block__volume');
-    const settingsButton: HTMLButtonElement = this.template.createBtn('exercise-block__settings');
+    const returnButton: HTMLButtonElement = this.template.createBtn(
+      "exercise-block__button-return"
+    );
+    const settings: HTMLElement = this.template.createElement(
+      "div",
+      "exercise-block__settings-block"
+    );
+    const volumeButton: HTMLButtonElement = this.template.createBtn(
+      "exercise-block__volume"
+    );
+    const settingsButton: HTMLButtonElement = this.template.createBtn(
+      "exercise-block__settings"
+    );
     settings.append(volumeButton, settingsButton);
     blockHeader.append(returnButton, settings);
   }
 
   private createExerciseInfo(): void {
-    const exerciseInfo: HTMLElement = this.template.createElement('div', 'exercise-block__info');
-    const exerciseName: HTMLElement = this.template.createElement('div', 'exercise-block__name', this.exercise.title);
-    const extraInfo: HTMLElement = this.template.createElement('div', 'exercise-block__extra');
-    const infoLink: HTMLAnchorElement = this.template.createLink('exercise-block__info-link', '#', 'Info');
-    const youtubeLink: HTMLAnchorElement = this.template.createLink('exercise-block__youtube', this.exercise.youtube, 'Watch');
-    youtubeLink.target = '_blank';
+    const exerciseInfo: HTMLElement = this.template.createElement(
+      "div",
+      "exercise-block__info"
+    );
+    const exerciseName: HTMLElement = this.template.createElement(
+      "div",
+      "exercise-block__name",
+      this.exercise.title
+    );
+    const extraInfo: HTMLElement = this.template.createElement(
+      "div",
+      "exercise-block__extra"
+    );
+    const infoLink: HTMLAnchorElement = this.template.createLink(
+      "exercise-block__info-link",
+      "#",
+      "Info"
+    );
+    const youtubeLink: HTMLAnchorElement = this.template.createLink(
+      "exercise-block__youtube",
+      this.exercise.youtube,
+      "Watch"
+    );
+    youtubeLink.target = "_blank";
     extraInfo.append(infoLink, youtubeLink);
     exerciseInfo.append(exerciseName, extraInfo);
     this.exerciseBlock.append(exerciseInfo, extraInfo);
   }
 
   private createCounter(): void {
-    const currentQuantity: HTMLElement = this.template.createElement('span', 'exercise-block__current-quantity', this.exercise.quantity);
+    const currentQuantity: HTMLElement = this.template.createElement(
+      "span",
+      "exercise-block__current-quantity",
+      this.exercise.quantity
+    );
     this.counter.append(currentQuantity);
 
-    if (this.exercise.quantity.toLowerCase().includes('x')) {
-      const doneButton: HTMLButtonElement = this.template.createBtn('exercise-block__button-done', 'Done');
+    if (this.exercise.quantity.toLowerCase().includes("x")) {
+      const doneButton: HTMLButtonElement = this.template.createBtn(
+        "exercise-block__button-done",
+        "Done"
+      );
       this.counter.append(doneButton);
     } else {
       const countdownBar: HTMLElement = this.createCountdownBar();
@@ -77,97 +117,179 @@ class ExerciseBlock {
   }
 
   private createCountdownBar(): HTMLElement {
-    const timeBar: HTMLElement = this.template.createElement('div', 'exercise-block__time-bar');
-    const track: HTMLElement = this.template.createElement('div', 'exercise-block__track');
-    const pauseButton = this.template.createBtn('exercise-block__button-pause', 'Pause');
-    const nextButton = this.template.createBtn('exercise-block__button-next', 'Next');
+    const timeBar: HTMLElement = this.template.createElement(
+      "div",
+      "exercise-block__time-bar"
+    );
+    const track: HTMLElement = this.template.createElement(
+      "div",
+      "exercise-block__track"
+    );
+    const pauseButton = this.template.createBtn(
+      "exercise-block__button-pause",
+      "Pause"
+    );
+    const nextButton = this.template.createBtn(
+      "exercise-block__button-next",
+      "Next"
+    );
     timeBar.append(track, pauseButton, nextButton);
     return timeBar;
   }
 
   private createNavigationButtons(): void {
-    const navigationButtons: HTMLElement = this.template.createElement('div', 'exercise-block__buttons');
-    const previousButton: HTMLButtonElement = this.template.createBtn('exercise-block__button-prev', 'Previous');
-    const delim: HTMLElement = this.template.createElement('div', 'exercise-block__delim');
-    const skipButton: HTMLButtonElement = this.template.createBtn('exercise-block__button-skip', 'Skip');
+    const navigationButtons: HTMLElement = this.template.createElement(
+      "div",
+      "exercise-block__buttons"
+    );
+    const previousButton: HTMLButtonElement = this.template.createBtn(
+      "exercise-block__button-prev",
+      "Previous"
+    );
+    const delim: HTMLElement = this.template.createElement(
+      "div",
+      "exercise-block__delim"
+    );
+    const skipButton: HTMLButtonElement = this.template.createBtn(
+      "exercise-block__button-skip",
+      "Skip"
+    );
     navigationButtons.append(previousButton, delim, skipButton);
     this.exerciseBlock.append(navigationButtons);
   }
 
   hideExerciseLinks(): void {
-    const linksBlock = <HTMLElement>document.querySelector('.exercise-block__extra');
-    linksBlock.style.visibility = 'hidden';
+    const linksBlock = <HTMLElement>(
+      document.querySelector(".exercise-block__extra")
+    );
+    linksBlock.style.visibility = "hidden";
   }
 
   showExerciseLinks(): void {
-    const linksBlock = <HTMLElement>document.querySelector('.exercise-block__extra');
+    const linksBlock = <HTMLElement>(
+      document.querySelector(".exercise-block__extra")
+    );
     if (linksBlock) {
-      linksBlock.style.visibility = 'visible';
+      linksBlock.style.visibility = "visible";
     }
   }
 
   showNextButton(): void {
-    const nextBtn = <HTMLButtonElement>document.querySelector('.exercise-block__button-next');
-    nextBtn.style.display = 'block';
+    const nextBtn = <HTMLButtonElement>(
+      document.querySelector(".exercise-block__button-next")
+    );
+    if (!nextBtn) {
+      return;
+    }
+    nextBtn.style.display = "block";
   }
 
   disablePreviousButton(): void {
-    const previousBtn = <HTMLButtonElement>document.querySelector('.exercise-block__button-prev');
-    previousBtn.classList.add('inactive');
+    const previousBtn = <HTMLButtonElement>(
+      document.querySelector(".exercise-block__button-prev")
+    );
+    if (!previousBtn) {
+      return;
+    }
+    previousBtn.classList.add("inactive");
     previousBtn.disabled = true;
   }
 
   enablePreviousButton(): void {
-    const previousBtn = <HTMLButtonElement>document.querySelector('.exercise-block__button-prev');
-    previousBtn.classList.remove('inactive');
+    const previousBtn = <HTMLButtonElement>(
+      document.querySelector(".exercise-block__button-prev")
+    );
+    if (!previousBtn) {
+      return;
+    }
+    previousBtn.classList.remove("inactive");
     previousBtn.disabled = false;
   }
 
   disableSkipButton(): void {
-    const skipBtn = <HTMLButtonElement>document.querySelector('.exercise-block__button-skip');
-    skipBtn.classList.add('inactive');
+    const skipBtn = <HTMLButtonElement>(
+      document.querySelector(".exercise-block__button-skip")
+    );
+    if (!skipBtn) {
+      return;
+    }
+    skipBtn.classList.add("inactive");
     skipBtn.disabled = true;
   }
 
   enableSkipButton(): void {
-    const skipBtn = <HTMLButtonElement>document.querySelector('.exercise-block__button-skip');
-    skipBtn.classList.remove('inactive');
+    const skipBtn = <HTMLButtonElement>(
+      document.querySelector(".exercise-block__button-skip")
+    );
+    if (!skipBtn) {
+      return;
+    }
+    skipBtn.classList.remove("inactive");
     skipBtn.disabled = false;
   }
 
   createThreeCount() {
-    document.body.style.pointerEvents = 'none';
-    const threeCounter: HTMLElement = this.template.createElement('div', 'counter-three');
+    document.body.style.pointerEvents = "none";
+    const threeCounter: HTMLElement = this.template.createElement(
+      "div",
+      "counter-three"
+    );
     this.counter.append(threeCounter);
     let count = 3;
     threeCounter.textContent = `${count}`;
-    threeCounter.classList.add('fading');
+    threeCounter.classList.add("fading");
     const updateCounter = () => {
       count = count - 1;
       threeCounter.textContent = `${count}`;
     };
-    threeCounter.addEventListener('animationiteration', updateCounter);
-    threeCounter.addEventListener('animationend', updateCounter);
+    threeCounter.addEventListener("animationiteration", updateCounter);
+    threeCounter.addEventListener("animationend", updateCounter);
   }
 
   createCountDown(): void {
-    const readyText = this.template.createElement('div', 'ready-text', 'ready to go');
-    const countdown: HTMLElement = this.template.createElement('div', 'countdown');
+    const readyText = this.template.createElement(
+      "div",
+      "ready-text",
+      "ready to go"
+    );
+    const countdown: HTMLElement = this.template.createElement(
+      "div",
+      "countdown"
+    );
     this.counter.append(readyText, countdown);
-    const circle: HTMLElement = this.template.createElement('div', 'countdown__circle');
+    const circle: HTMLElement = this.template.createElement(
+      "div",
+      "countdown__circle"
+    );
     countdown.append(circle);
-    const countDot: HTMLElement = this.template.createElement('div', 'countdown__dot');
-    const countSvg: SVGElement = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-    const svgCircle1: SVGCircleElement = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-    svgCircle1.setAttribute('cx', '70');
-    svgCircle1.setAttribute('cy', '70');
-    svgCircle1.setAttribute('r', '70');
-    const svgCircle2: SVGCircleElement = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-    svgCircle2.setAttribute('cx', '70');
-    svgCircle2.setAttribute('cy', '70');
-    svgCircle2.setAttribute('r', '70');
+    const countDot: HTMLElement = this.template.createElement(
+      "div",
+      "countdown__dot"
+    );
+    const countSvg: SVGElement = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "svg"
+    );
+    const svgCircle1: SVGCircleElement = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "circle"
+    );
+    svgCircle1.setAttribute("cx", "70");
+    svgCircle1.setAttribute("cy", "70");
+    svgCircle1.setAttribute("r", "70");
+    const svgCircle2: SVGCircleElement = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "circle"
+    );
+    svgCircle2.setAttribute("cx", "70");
+    svgCircle2.setAttribute("cy", "70");
+    svgCircle2.setAttribute("r", "70");
     countSvg.append(svgCircle1, svgCircle2);
-    const countNumber = this.template.createElement('div', 'countdown__number', '10');
+    const countNumber = this.template.createElement(
+      "div",
+      "countdown__number",
+      "10"
+    );
 
     circle.append(countDot, countSvg, countNumber);
 
@@ -175,11 +297,11 @@ class ExerciseBlock {
       const curNumber = Number(countNumber.textContent) - 1;
       if (curNumber === 0) {
         clearInterval(counter);
-        countdown.style.display = 'none';
-        readyText.style.display = 'none';
+        countdown.style.display = "none";
+        readyText.style.display = "none";
         this.showExerciseLinks();
         this.enableSkipButton();
-        document.body.style.pointerEvents = '';
+        document.body.style.pointerEvents = "";
       }
       countNumber.textContent = `${curNumber}`;
       svgCircle2.style.strokeDashoffset = `${440 - (440 * curNumber) / 10}`;

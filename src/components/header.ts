74e67950a51afcb317db228ace271bb674fbd449
oolenkazolot <oolenka.zolot@gmail.com>
2023-02-26 19:46:04 +0100

    fix: remove pointer-events: none in startTraining.ts

--- src/components/header.ts
+++ src/components/header.ts
@@ -1,27 +1,24 @@
-import Template from "../templates/template";
-import { ITemplate } from "../types/index";
-import Button from "../components/Button";
-import { onOpenModal } from "../utils/component-utils";
-import router from "./routerComponent";
-import {
-  getUserTokenLocalStorage,
-  removeUserLocalStorage,
-} from "../utils/auth";
+import Template from '../templates/template';
+import { ITemplate } from '../types/index';
+import Button from '../components/Button';
+import { onOpenModal } from '../utils/component-utils';
+import router from './routerComponent';
+import { getUserTokenLocalStorage, removeUserLocalStorage } from '../utils/auth';
 
 class Header {
   template: ITemplate;
   mainClass: string;
   constructor() {
     this.template = new Template();
-    this.mainClass = "header";
+    this.mainClass = 'header';
   }
 
   public draw(): void {
-    const header: HTMLElement | null = document.querySelector("header");
+    const header: HTMLElement | null = document.querySelector('header');
     if (!header) {
       return;
     }
-    header.textContent = "";
+    header.textContent = '';
     const isAuth = getUserTokenLocalStorage();
 
     header.classList.add(`${this.mainClass}`);
@@ -36,36 +33,29 @@ class Header {
   }
 
   private createLogo(): HTMLElement {
-    const logo: HTMLAnchorElement = this.template.createLink("logo", "/");
-    const spanOne: HTMLElement = this.template.createElement(
-      "span",
-      "logo__content",
-      "Women Workouts"
-    );
-    const spanTwo: HTMLElement = this.template.createElement(
-      "span",
-      "logo__content",
-      "Women Workouts"
-    );
+    const logo: HTMLAnchorElement = this.template.createLink('logo', '/');
+    const spanOne: HTMLElement = this.template.createElement('span', 'logo__content', 'Women Workouts');
+    const spanTwo: HTMLElement = this.template.createElement('span', 'logo__content', 'Women Workouts');
     logo.append(spanOne, spanTwo);
+    logo.addEventListener('click', (e) => {
+      e.preventDefault();
+      router.navigate('');
+    });
     return logo;
   }
 
   private createButtons(): HTMLElement {
-    const buttons: HTMLElement = this.template.createElement(
-      "div",
-      `${this.mainClass}__buttons`
-    );
+    const buttons: HTMLElement = this.template.createElement('div', `${this.mainClass}__buttons`);
     const btnSignIn: HTMLButtonElement = Button({
-      content: "Sign In",
+      content: 'Sign In',
       className: [`${this.mainClass}__btn`],
-      onClick: onOpenModal("modal-sign-in"),
+      onClick: onOpenModal('modal-sign-in'),
     });
     const btnSignUp: HTMLButtonElement = Button({
-      content: "Sign Up",
+      content: 'Sign Up',
       className: [`${this.mainClass}__btn`],
-      variant: "second",
-      onClick: onOpenModal("modal-sign-up"),
+      variant: 'second',
+      onClick: onOpenModal('modal-sign-up'),
     });
 
     buttons.append(btnSignIn, btnSignUp);
@@ -74,7 +64,7 @@ class Header {
 
   private createBtnSignOut(): HTMLButtonElement {
     const btnSignOut: HTMLButtonElement = Button({
-      content: "Sign Out",
+      content: 'Sign Out',
       className: [`${this.mainClass}__btn`],
       onClick: this.exitApp.bind(this),
     });
@@ -82,18 +72,12 @@ class Header {
   }
 
   private createWrap(): HTMLElement {
-    const wrap: HTMLElement = this.template.createElement(
-      "div",
-      `${this.mainClass}__wrap`
-    );
+    const wrap: HTMLElement = this.template.createElement('div', `${this.mainClass}__wrap`);
     return wrap;
   }
 
   private createListMenu(): HTMLElement {
-    const list: HTMLElement = this.template.createElement(
-      "ul",
-      `${this.mainClass}__list`
-    );
+    const list: HTMLElement = this.template.createElement('ul', `${this.mainClass}__list`);
     const linksMenu: HTMLElement[] = this.createLinksMenu();
     list.append(...linksMenu);
     return list;
@@ -101,12 +85,9 @@ class Header {
 
   private createLinksMenu(): HTMLElement[] {
     const linksMenu: HTMLElement[] = [];
-    const nameLinksMenu = ["workouts", "exercises", "profile"];
+    const nameLinksMenu = ['workouts', 'exercises', 'profile'];
     nameLinksMenu.forEach((nameLink) => {
-      const listItem: HTMLElement = this.template.createElement(
-        "li",
-        `${this.mainClass}__list-item`
-      );
+      const listItem: HTMLElement = this.template.createElement('li', `${this.mainClass}__list-item`);
       const link: HTMLAnchorElement = this.createLink(nameLink);
       listItem.append(link);
       linksMenu.push(listItem);
@@ -115,15 +96,11 @@ class Header {
   }
 
   private createLink(name: string): HTMLAnchorElement {
-    const link: HTMLAnchorElement = this.template.createLink(
-      `${this.mainClass}__link`,
-      "/",
-      name
-    );
+    const link: HTMLAnchorElement = this.template.createLink(`${this.mainClass}__link`, '/', name);
     if (router.isActiveRout(name)) {
-      link.classList.add("active");
+      link.classList.add('active');
     }
-    link.addEventListener("click", (e) => {
+    link.addEventListener('click', (e) => {
       this.onClickHandlerLinkMenu(e, name);
     });
     link.append(this.createBorderLink());
@@ -131,10 +108,7 @@ class Header {
   }
 
   private createBorderLink(): HTMLElement {
-    const borderLink: HTMLElement = this.template.createElement(
-      "span",
-      `${this.mainClass}__link-border`
-    );
+    const borderLink: HTMLElement = this.template.createElement('span', `${this.mainClass}__link-border`);
     return borderLink;
   }
 
@@ -146,7 +120,7 @@ class Header {
   private exitApp() {
     removeUserLocalStorage();
     this.draw();
-    router.navigate("");
+    router.navigate('');
   }
 }
 
--- src/pages/startTraining.ts
+++ src/pages/startTraining.ts
@@ -1,11 +1,11 @@
-import Template from "../templates/template";
-import { IExercise, ITemplate } from "../types/index";
-import TakeARest from "../components/takeaRest";
-import ExerciseBlock from "../components/exerciseBlock";
-import PauseModal from "../components/pauseModal";
-import workout_plans from "../utils/workout-plans-en";
-import Congrats from "../components/congrats";
-import router from "../components/routerComponent";
+import Template from '../templates/template';
+import { IExercise, ITemplate } from '../types/index';
+import TakeARest from '../components/takeaRest';
+import ExerciseBlock from '../components/exerciseBlock';
+import PauseModal from '../components/pauseModal';
+import workout_plans from '../utils/workout-plans-en';
+import Congrats from '../components/congrats';
+import router from '../components/routerComponent';
 
 class StartTrainingPage {
   template: ITemplate;
@@ -25,18 +25,18 @@ class StartTrainingPage {
   }
 
   public draw(): void {
-    const mainElement: HTMLElement | null = document.querySelector("main");
+    const mainElement: HTMLElement | null = document.querySelector('main');
     if (!mainElement) {
       return;
     }
-    mainElement.classList.add("main");
+    mainElement.classList.add('main');
 
-    mainElement.textContent = "";
-    const mainPageElement: HTMLElement = document.createElement("div");
-    mainPageElement.classList.add("startTraining-page");
+    mainElement.textContent = '';
+    const mainPageElement: HTMLElement = document.createElement('div');
+    mainPageElement.classList.add('startTraining-page');
     mainElement.append(mainPageElement);
 
-    const complexId: string = localStorage.complexId || "1";
+    const complexId: string = localStorage.complexId || '1';
     workout_plans.forEach((plan) => {
       plan.block.forEach((item) => {
         if (item.id === Number(complexId)) {
@@ -46,18 +46,11 @@ class StartTrainingPage {
     });
 
     this.currentExerciseIndex = 0;
-    const curExercise = new ExerciseBlock(
-      this.exerciseArray[this.currentExerciseIndex]
-    );
+    const curExercise = new ExerciseBlock(this.exerciseArray[this.currentExerciseIndex]);
     mainPageElement.append(curExercise.draw());
-    if (
-      this.currentExerciseIndex === 0 &&
-      this.exerciseArray[this.currentExerciseIndex].quantity
-        .toLowerCase()
-        .includes("x")
-    ) {
+    if (this.currentExerciseIndex === 0 && this.exerciseArray[this.currentExerciseIndex].quantity.toLowerCase().includes('x')) {
       curExercise.createCountDown();
-      document.body.style.pointerEvents = "none";
+
       curExercise.hideExerciseLinks();
       curExercise.disablePreviousButton();
       curExercise.disableSkipButton();
@@ -65,25 +58,23 @@ class StartTrainingPage {
       clearInterval(this.interval);
       curExercise.createThreeCount();
       setTimeout(() => {
-        const duration = this.getExerciseDuration(
-          this.exerciseArray[this.currentExerciseIndex]
-        );
+        const duration = this.getExerciseDuration(this.exerciseArray[this.currentExerciseIndex]);
         this.setTimeCounter(duration);
-        document.body.style.pointerEvents = "";
+        document.body.style.pointerEvents = '';
       }, 3000);
       curExercise.disablePreviousButton();
     }
 
     const start = Date.now();
 
-    document.addEventListener("click", (e) => {
+    document.addEventListener('click', (e) => {
       const target = <HTMLButtonElement>e.target;
-      if (target.classList.contains("exercise-block__button-done")) {
+      if (target.classList.contains('exercise-block__button-done')) {
         clearInterval(this.interval);
         this.showRestModal();
         this.counter++;
       }
-      if (target.classList.contains("exercise-block__button-next")) {
+      if (target.classList.contains('exercise-block__button-next')) {
         if (this.currentExerciseIndex === this.exerciseArray.length - 1) {
           clearInterval(this.interval);
           this.counter++;
@@ -95,7 +86,7 @@ class StartTrainingPage {
           this.counter++;
         }
       }
-      if (target.classList.contains("exercise-block__button-skip")) {
+      if (target.classList.contains('exercise-block__button-skip')) {
         if (this.currentExerciseIndex === this.exerciseArray.length - 1) {
           clearInterval(this.interval);
           const resultMins = this.getResultMinutes(start);
@@ -105,11 +96,11 @@ class StartTrainingPage {
           this.loadNextExercise();
         }
       }
-      if (target.classList.contains("rest__skip-btn")) {
+      if (target.classList.contains('rest__skip-btn')) {
         clearInterval(this.interval);
         this.loadNextExercise();
       }
-      if (target.classList.contains("exercise-block__button-prev")) {
+      if (target.classList.contains('exercise-block__button-prev')) {
         if (this.currentExerciseIndex === 0) {
           return;
         } else {
@@ -117,37 +108,30 @@ class StartTrainingPage {
           this.loadPreviousExercise();
         }
       }
-      if (target.classList.contains("pause-modal__button-restart")) {
+      if (target.classList.contains('pause-modal__button-restart')) {
         this.closeModal();
         clearInterval(this.interval);
-        document.body.style.overflow = "";
+        document.body.style.overflow = '';
         this.restartExercise();
       }
-      if (target.classList.contains("exercise-block__button-pause")) {
+      if (target.classList.contains('exercise-block__button-pause')) {
         clearInterval(this.interval);
-        document.body.style.overflow = "hidden";
-        const modal = new PauseModal(
-          this.exerciseArray[this.currentExerciseIndex]
-        );
+        document.body.style.overflow = 'hidden';
+        const modal = new PauseModal(this.exerciseArray[this.currentExerciseIndex]);
         modal.draw();
       }
-      if (target.classList.contains("pause-modal__button-continue")) {
+      if (target.classList.contains('pause-modal__button-continue')) {
         this.closeModal();
-        document.body.style.overflow = "";
+        document.body.style.overflow = '';
         clearInterval(this.interval);
         this.createThreeCount();
         setTimeout(() => {
-          const duration = this.getExerciseDuration(
-            this.exerciseArray[this.currentExerciseIndex]
-          );
+          const duration = this.getExerciseDuration(this.exerciseArray[this.currentExerciseIndex]);
           this.setTimeCounter(duration);
-          document.body.style.pointerEvents = "";
+          document.body.style.pointerEvents = '';
         }, 3000);
       }
-      if (
-        target.classList.contains("header__link") ||
-        target.classList.contains("header__btn")
-      ) {
+      if (target.classList.contains('header__link') || target.classList.contains('header__btn')) {
         clearInterval(this.interval);
       }
       e.preventDefault();
@@ -175,66 +159,50 @@ class StartTrainingPage {
   }
 
   private showExercise(): void {
-    const page = <HTMLElement>document.querySelector(".startTraining-page");
-    const curExercise = new ExerciseBlock(
-      this.exerciseArray[this.currentExerciseIndex]
-    );
-    if (
-      !this.exerciseArray[this.currentExerciseIndex].quantity
-        .toLowerCase()
-        .includes("x")
-    ) {
+    const page = <HTMLElement>document.querySelector('.startTraining-page');
+    const curExercise = new ExerciseBlock(this.exerciseArray[this.currentExerciseIndex]);
+    if (!this.exerciseArray[this.currentExerciseIndex].quantity.toLowerCase().includes('x')) {
       clearInterval(this.interval);
       curExercise.createThreeCount();
       setTimeout(() => {
-        const duration = this.getExerciseDuration(
-          this.exerciseArray[this.currentExerciseIndex]
-        );
+        const duration = this.getExerciseDuration(this.exerciseArray[this.currentExerciseIndex]);
         this.setTimeCounter(duration);
-        document.body.style.pointerEvents = "";
+        document.body.style.pointerEvents = '';
       }, 3000);
     }
-    page.innerHTML = "";
+    page.innerHTML = '';
     page.append(curExercise.draw());
   }
 
   private showRestModal(): void {
-    const pageContent = <HTMLElement>(
-      document.querySelector(".startTraining-page")
-    );
-    pageContent.innerHTML = "";
-    pageContent.append(
-      this.takeARest.draw(this.currentExerciseIndex, this.exerciseArray)
-    );
+    const pageContent = <HTMLElement>document.querySelector('.startTraining-page');
+    pageContent.innerHTML = '';
+    pageContent.append(this.takeARest.draw(this.currentExerciseIndex, this.exerciseArray));
     this.autoChange();
   }
 
   private showCongrats(counter: number, time: number): void {
-    const pageContent = <HTMLElement>(
-      document.querySelector(".startTraining-page")
-    );
+    const pageContent = <HTMLElement>document.querySelector('.startTraining-page');
     const congrats = new Congrats(counter, time);
-    pageContent.innerHTML = "";
+    pageContent.innerHTML = '';
     pageContent.append(congrats.draw());
 
-    const completeBtn = <HTMLAnchorElement>(
-      document.querySelector(".congrats__button-complete")
-    );
-    completeBtn.addEventListener("click", (e) => {
+    const completeBtn = <HTMLAnchorElement>document.querySelector('.congrats__button-complete');
+    completeBtn.addEventListener('click', (e) => {
       e.preventDefault();
-      const mainElement: HTMLElement | null = document.querySelector("main");
+      const mainElement: HTMLElement | null = document.querySelector('main');
       if (mainElement) {
-        mainElement.innerHTML = "";
-        router.navigate("exercises");
+        mainElement.innerHTML = '';
+        router.navigate('exercises');
       }
     });
   }
 
   private autoChange(): void {
     let counter = 0;
-    const addBtn = document.querySelector(".rest__add-btn") as HTMLElement;
-    const skipBtn = document.querySelector(".rest__skip-btn") as HTMLElement;
-    addBtn.addEventListener("click", () => {
+    const addBtn = document.querySelector('.rest__add-btn') as HTMLElement;
+    const skipBtn = document.querySelector('.rest__skip-btn') as HTMLElement;
+    addBtn.addEventListener('click', () => {
       counter = counter - 20;
     });
     const int = setInterval(() => {
@@ -247,7 +215,7 @@ class StartTrainingPage {
         }
       }
     }, 1000);
-    skipBtn.addEventListener("click", () => {
+    skipBtn.addEventListener('click', () => {
       counter = 0;
       clearInterval(int);
     });
@@ -256,23 +224,19 @@ class StartTrainingPage {
   setTimeCounter(duration: number): void {
     clearInterval(this.interval);
     this.interval = setInterval(() => {
-      const quantity = <HTMLElement>(
-        document.querySelector(".exercise-block__current-quantity")
-      );
+      const quantity = <HTMLElement>document.querySelector('.exercise-block__current-quantity');
       const timeLeft = quantity.textContent;
       let minutes = timeLeft?.slice(0, 2);
       let seconds = timeLeft?.slice(3);
       if (Number(seconds) === 0) {
         if (Number(minutes) === 0) {
           clearInterval(this.interval);
-          const nextBtn = <HTMLButtonElement>(
-            document.querySelector(".exercise-block__button-next")
-          );
-          nextBtn.style.display = "block";
+          const nextBtn = <HTMLButtonElement>document.querySelector('.exercise-block__button-next');
+          nextBtn.style.display = 'block';
         }
         if (Number(minutes) > 0 && Number(minutes) <= 10) {
           minutes = `0${Number(minutes) - 1}`;
-          seconds = "59";
+          seconds = '59';
         }
       } else {
         seconds = `${Number(seconds) - 1}`;
@@ -281,13 +245,9 @@ class StartTrainingPage {
         }
       }
       quantity.textContent = `${minutes}:${seconds}`;
-      const widthEl = <HTMLElement>(
-        document.querySelector(".exercise-block__time-bar")
-      );
+      const widthEl = <HTMLElement>document.querySelector('.exercise-block__time-bar');
       const maxWidth = widthEl.clientWidth;
-      const track = <HTMLElement>(
-        document.querySelector(".exercise-block__track")
-      );
+      const track = <HTMLElement>document.querySelector('.exercise-block__track');
       track.style.width = `${(maxWidth / duration) * Number(seconds)}px`;
     }, 1000);
   }
@@ -295,36 +255,27 @@ class StartTrainingPage {
   getExerciseDuration(exercise: IExercise): number {
     const durMins = Number(exercise.quantity.slice(0, 2));
     const durSeconds = Number(exercise.quantity.slice(3));
-    const duration: number =
-      durMins !== 0 ? durMins * 60 + durSeconds : durSeconds;
+    const duration: number = durMins !== 0 ? durMins * 60 + durSeconds : durSeconds;
     return duration;
   }
 
   createThreeCount(): void {
-    document.body.style.pointerEvents = "none";
-    const threeCounter: HTMLElement = this.template.createElement(
-      "div",
-      "counter-three"
-    );
-    const counter = <HTMLElement>(
-      document.querySelector(".exercise-block__counter")
-    );
+    const threeCounter: HTMLElement = this.template.createElement('div', 'counter-three');
+    const counter = <HTMLElement>document.querySelector('.exercise-block__counter');
     counter.append(threeCounter);
     let count = 3;
     threeCounter.textContent = `${count}`;
-    threeCounter.classList.add("fading");
+    threeCounter.classList.add('fading');
     const updateCounter = () => {
       count = count - 1;
       threeCounter.textContent = `${count}`;
     };
-    threeCounter.addEventListener("animationiteration", updateCounter);
-    threeCounter.addEventListener("animationend", updateCounter);
+    threeCounter.addEventListener('animationiteration', updateCounter);
+    threeCounter.addEventListener('animationend', updateCounter);
   }
 
   closeModal() {
-    const backLayer = <HTMLElement>(
-      document.querySelector(".pause-modal__backlayer")
-    );
+    const backLayer = <HTMLElement>document.querySelector('.pause-modal__backlayer');
     document.body.removeChild(backLayer);
   }
 }
